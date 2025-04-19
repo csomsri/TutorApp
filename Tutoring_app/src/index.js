888fa8403aadc469499a6c56d7b048e1808ad9fd
Samar <75354890+Samar-777@users.noreply.github.com>
2025-04-19 14:45:56 -0700

    Add files via upload

---
+++ Tutoring_app/src/index.js
@@ -0,0 +1,47 @@
+import {useState } from 'react'
+import { View, ScrollView, SafeAreaView, Text } from 'react-native';
+import { Stack, useRouter } from 'expo-router'
+
+import {COLORS, icons, images, SIZES } from '../constants'
+import { Home, leaderboard, ScreenHeaderBtn, , Welcome } 
+from '../components'
+
+const Home = () => {
+    const router = useRouter();
+
+    return (
+        <SafeAreaView style={{flex: 200, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen 
+            options={{
+                headerStyle: {backgroundColor: COLORS.gray2}, 
+                headerShadowVisible: false,
+            headerLeft: () => (
+                <ScreenHeaderBtn iconUrl = {icons.menu} dimension = "60%" style={{ marginLeft: 10}} />
+            ),
+            headerRight: () => (
+                <ScreenHeaderBtn iconUrl = {images.profile} dimension = "100%" style={{ marginRight: 10}}/>
+            ),
+            headerTitle: ""
+        }} />
+
+        <ScrollView showsVerticalScrollIndicator={false}>
+            <View 
+                style={{
+                    flex:1,
+                    padding: SIZES.medium
+             }}>
+                <Welcome
+
+                />
+                <Popularjobs />
+                <Nearbyjobs />
+                
+             </View>
+        </ScrollView>
+
+            
+        </SafeAreaView>
+    )
+}
+
+export default Home;
